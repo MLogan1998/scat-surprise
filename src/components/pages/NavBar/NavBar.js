e1c03a015f49085a4dd768cdec07500f200e25aa
Matt Logan <mlogan5212@gmail.com>
2020-08-29 11:22:22 -0500

    import reactstrap navbar

--- src/components/pages/NavBar/NavBar.js
+++ src/components/pages/NavBar/NavBar.js
@@ -1,5 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -9,6 +24,10 @@ import './NavBar.scss';
 import Auth from '../Auth/Auth';
 
 class NavBar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
   static propTypes = {
     authed: PropTypes.bool,
   }
@@ -18,15 +37,55 @@ class NavBar extends React.Component {
     firebase.auth().signOut();
   }
 
+  toggle = () => {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
     const { authed } = this.props;
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#"><i class="fas fa-crow fa-2x"></i></a>
-      <ul className = "ml-auto">
-      <Auth authed={authed}/>
-      </ul>
-      </nav>
+    //   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    //   <a className="navbar-brand" href="#"><i class="fas fa-crow fa-2x"></i></a>
+    //   <ul className = "ml-auto">
+    //   <Auth authed={authed}/>
+    //   </ul>
+    //   </nav>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
     );
   }
 }
