1f116d2aac3b842bffa5aba729ad41c8c49fc28c
Matt Logan <mlogan5212@gmail.com>
2020-08-29 09:56:43 -0500

    added navbar, firebase config, google auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import authData from '../helpers/data/authData';
+import connection from '../helpers/data/connection';
+
+import NavBar from '../javascripts/components/pages/NavBar/NavBar';
+import Home from '../javascripts/components/pages/Home/Home';
+
 import './App.scss';
 
+connection();
+
 class App extends React.Component {
+  state ={
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      if (authed) {
+        const uid = authData.getUid();
+        return <Home uid={uid}/>;
+      }
+      return '';
+    };
+
     return (
       <div className="App">
-        <h2>Scat Surprise</h2>
-        <button className="btn btn-info">I am a button</button>
+        <NavBar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
---
+++ src/javascripts/components/pages/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './NavBar.scss';
+
+import Auth from '../Auth/Auth';
+
+class NavBar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  }
+
+  signOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
+  render() {
+    const { authed } = this.props;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#"><i class="fas fa-crow fa-2x"></i></a>
+      <ul className = "ml-auto">
+      <Auth authed={authed}/>
+      </ul>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
